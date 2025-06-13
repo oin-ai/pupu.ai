4fa15f8a13612c0d337a8321d73b5b39b17f0485
wucjgit <470013468@qq.com>
2025-06-13 12:05:45 +0800

    fix: bugs

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -113,6 +113,9 @@ export default function PrivacyPage() {
           If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at [Contact Email Address].
         </p>
       </div>
+
+      <a href="story_ai_app://story?story_id=[故事ID的值]&title=[标题的值]&content=[内容的值]&pic_link=[图片链接的值]&audio_link=https://zk.work/download/test-audio.mp3&voice_name=[音色名称的值]
+">打开 Story Ai</a>
     </div>
   );
 }
